9a02a8da4a5d7206dc89798d6ecd1add37b52781
aelnko <lekolupaeva1201@gmail.com>
2024-06-17 16:35:02 +0300

    fix router

--- src/RouterWrapper.jsx
+++ src/RouterWrapper.jsx
@@ -1,14 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-import Cats from './pages/Cats';
-import Dogs from './pages/Dogs';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Cats from "./pages/Cats";
+import Dogs from "./pages/Dogs";
 
 const RouterWrapper = () => {
   return (
-      <Routes>
-        <Route path="/cats" element={<Cats />} />
-        <Route path="/dogs" element={<Dogs />}/>
-      </Routes>
-  )
+    <Routes>
+      <Route path="/cats" element={<Cats />} />
+      <Route path="/dogs" element={<Dogs />} />
+      <Route path="/" element={<Navigate to="/cats"/>}></Route>
+    </Routes>
+  );
 };
 
 export default RouterWrapper;
